77da997bfec009857219990287f8cff072dd6bff
AdirthaBorgohain <adirthaborgohain@gmail.com>
2021-09-18 21:21:25 +0530

    added streamlit MCR script

---
+++ BERTMCR.py
@@ -0,0 +1,71 @@
+from tqdm import tqdm
+import streamlit as st
+import tensorflow as tf
+from typing import List, Dict
+from transformers import AutoTokenizer, TFAutoModelForQuestionAnswering
+
+
+class BERTMCR:
+    def __init__(self, naq_threshold: float = 2.0):
+        self.__bert_model = "bert-large-uncased-whole-word-masking-finetuned-squad"
+        self.__tokenizer = AutoTokenizer.from_pretrained(self.__bert_model)
+        self.__model = TFAutoModelForQuestionAnswering.from_pretrained(self.__bert_model)
+        self.__naq_threshold = naq_threshold
+
+    def __classify_question(self, start_scores, end_scores) -> bool:
+        score = min(max(start_scores[0]), max(end_scores[0]))
+        if score < self.__naq_threshold:
+            return False
+        else:
+            return True
+
+    def extract_answers(self, context: str, questions: List[str]) -> Dict[str, List]:
+        res_dict, answers = dict(), list()
+        res_dict['context'] = context
+        for question in tqdm(questions, desc="Extracting answers from context..."):
+            res = dict()
+            res['question'] = question
+            inputs = self.__tokenizer(question, context, add_special_tokens=True, return_tensors="tf", max_length=512,
+                                      truncation=True)
+            input_ids = inputs["input_ids"].numpy()[0]
+            outputs = self.__model(inputs)
+            answer_start_scores = outputs.start_logits
+            answer_end_scores = outputs.end_logits
+
+            res['is_answerable'] = self.__classify_question(answer_start_scores, answer_end_scores)
+            if res['is_answerable']:
+                # Get the most likely beginning and ends of answer with the argmax of the score
+                answer_start = tf.argmax(answer_start_scores, axis=1).numpy()[0]
+                answer_end = (tf.argmax(answer_end_scores, axis=1) + 1).numpy()[0]
+                answer = self.__tokenizer.convert_tokens_to_string(
+                    self.__tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+                res['extracted_answer'] = answer.capitalize()
+            else:
+                res['extracted_answer'] = ""
+            answers.append(res)
+        res_dict['answers'] = answers
+        return res_dict
+
+@st.cache(hash_funcs={BERTMCR: lambda _: None}, allow_output_mutation=True)
+def load_model():
+    return BERTMCR()
+
+MCR = load_model()
+
+st.title("Machine Comprehension Reading Model")
+st.write("A model to get answers from questions given a context. Powered by Google's BERT.")
+
+with st.form(key='input_form'):
+    context = st.text_area('Context', height=100, help="Enter context here...")
+    question = st.text_input('Question', help='All questions must be answerable based on the context.')
+    submit_button = st.form_submit_button(label='Get Answer')
+    if submit_button:
+        res = MCR.extract_answers(context=context, questions=[question])
+        st.text(" \n")
+        st.text(" \n")
+        st.markdown('**Answer:**')
+        if res['answers'][0]['extracted_answer']:
+            st.write(res['answers'][0]['extracted_answer'])
+        else:
+            st.write('Could not answer question.')
+            #    st.write(f"Answer: {res['answers'][0]['extracted_answer']}")
